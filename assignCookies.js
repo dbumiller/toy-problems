cf442d17727673c4a8e536b1f9d959c9cf37b42a
dbumiller <d.m.bumiller@gmail.com>
2021-07-05 23:13:21 -0700

    Found a way to make assign cookies work with huge sample sizes, but it is rather slow

--- assignCookies.js
+++ assignCookies.js
@@ -4,31 +4,22 @@
  * @return {number}
  */
 var findContentChildren = function(g, s) {
-  //create an output variable
-  //loop through cookies
-    //for each cookie, find the hardest to please recipient and give them the cookie
-    //if such a recipient exists, add one to output variable
+
+  g.sort((a, b) => a-b);
+  s.sort((a, b) => a-b);
 
   var content = 0;
+
+
   for (var i = 0; i < s.length; i++) {
-      var size = s[i];
-      for (var j = size; j > 0; j--) {
-          var found = false;
-          for (var k = 0; k < g.length; k++) {
-              if (g[k] === j) {
-                  content++;
-                  g[k] = Infinity;
-                  found = true;
-                  break;
-              }
-          }
-          if (found) {
+      for (var j = 0; j < g.length; j++) {
+          if (s[i] >= g[j]) {
+              content++;
+              g[j] = Infinity;
+              j++;
               break;
           }
       }
   }
   return content;
-
-
-
 };
